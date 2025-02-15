b0d71679c6a3db73b51ba7ce15ea38472924eacd
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:23:35 -0600

    ejercicio 1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int numeroEntero = 42;
+float numeroFlotante = 3.14f;
+double numeroDoble = 2.7182818284;
+char caracter = 'A';
+string texto = "Hola, mundo!";
+bool esVerdadero = true;
+
+Console.WriteLine("Valor de int: " + numeroEntero);
+Console.WriteLine("Valor de float: " + numeroFlotante);
+Console.WriteLine("Valor de double: " + numeroDoble);
+Console.WriteLine("Valor de char: " + caracter);
+Console.WriteLine("Valor de string: " + texto);
+Console.WriteLine("Valor de bool: " + esVerdadero);
+
+Console.WriteLine("El Ing Daro es muy feo.");
+
+
+
+Console.WriteLine("El Ing Daro es muy Bonito pero el ejercicio me pide que diga que es muy feo :(((((.");
